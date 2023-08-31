e8ee83f0119bd7f7558bc275a701b5f71d317b5d
JY LEE <wndyd0110@gmail.com>
2023-08-31 22:06:14 +0900

    Complete RemoveBoard

--- src/main/java/org/board/mvc/noticeboard/restcontroller/BoardRestController.java
+++ src/main/java/org/board/mvc/noticeboard/restcontroller/BoardRestController.java
@@ -3,7 +3,9 @@ package org.board.mvc.noticeboard.restcontroller;
 import org.board.mvc.noticeboard.dto.RegistBoardDTO;
 import org.board.mvc.noticeboard.service.BoardService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,9 @@ public class BoardRestController {
     // 의존성 주입
     private final BoardService boardService;
 
+    // 게시판 등록
     @PostMapping("/regist")
-    public Long registBoard(@RequestBody RegistBoardDTO registBoardDTO){
+    public Long registBoard(@RequestBody RegistBoardDTO registBoardDTO) {
 
         log.info("=======================");
         log.info("POST | /api/board/regist");
@@ -31,8 +34,22 @@ public class BoardRestController {
 
         Long bno = boardService.registBoard(registBoardDTO);
 
-        return  bno;
+        return bno;
 
     }
-    
+
+    // 게시판 삭제
+    @PutMapping("/remove/{bno}")
+    public Long removeBoard(
+            @PathVariable("bno") Long bno) {
+
+        log.info("=======================");
+        log.info("POST | /api/board/remove/", bno);
+        log.info("=======================");
+
+        boardService.removeBoard(bno);
+
+        return bno;
+    }
+
 }
