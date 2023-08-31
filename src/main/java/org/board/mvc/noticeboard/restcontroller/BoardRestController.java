0dac8440fb8a8ab85b675e74ca056f4deddd9cf3
JY LEE <wndyd0110@gmail.com>
2023-08-31 21:59:12 +0900

    Complete RegistBoardController

--- src/main/java/org/board/mvc/noticeboard/restcontroller/BoardRestController.java
+++ src/main/java/org/board/mvc/noticeboard/restcontroller/BoardRestController.java
@@ -1,5 +1,38 @@
 package org.board.mvc.noticeboard.restcontroller;
 
+import org.board.mvc.noticeboard.dto.RegistBoardDTO;
+import org.board.mvc.noticeboard.service.BoardService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@Log4j2
+@RequestMapping("/api/board")
 public class BoardRestController {
+
+    // 의존성 주입
+    private final BoardService boardService;
+
+    @PostMapping("/regist")
+    public Long registBoard(@RequestBody RegistBoardDTO registBoardDTO){
+
+        log.info("=======================");
+        log.info("POST | /api/board/regist");
+        log.info(registBoardDTO);
+        log.info("=======================");
+
+        Long bno = boardService.registBoard(registBoardDTO);
+
+        return  bno;
+
+    }
     
 }
